1a115551beb6a18753573564f81c4aec96577248
Suhail Edipoglu <suhail.edipoglu@gmail.com>
2024-01-10 10:11:26 +0100

    Projektdateien hinzufügen.

---
+++ Project1/Game1.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
+using SharpDX.Direct2D1;
+using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
+using System.Drawing;
+using Color = Microsoft.Xna.Framework.Color;
+using Microsoft.VisualBasic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Project1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        GridDrawer gridDrawer;
+        List<List<int>> grid;
+        int gridHeight = 50;
+        int gridWidth = 100;
+        double updateInterval = 0.0; // Update every 1 second
+        double timeSinceLastUpdate = 0.0;
+
+        SpriteFont font;
+        string text = "Generation: ";
+        int generation = 0;
+        Vector2 position = new Vector2(0, 0);
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 1000;  // set this to the desired width
+            _graphics.PreferredBackBufferHeight = 520; // set this to the desired height
+            _graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            grid = new List<List<int>>();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            FillGrid(gridHeight, gridWidth);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            gridDrawer = new GridDrawer(GraphicsDevice);
+
+            // Load the font in the LoadContent method
+            font = Content.Load<SpriteFont>("NewFont");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                grid.Clear();
+                FillGrid(gridHeight, gridWidth);
+                generation = 0;
+            }
+
+            timeSinceLastUpdate += gameTime.ElapsedGameTime.TotalSeconds;
+            if (timeSinceLastUpdate > updateInterval)
+            {
+
+                // TODO: Add your update logic here
+                gridDrawer.UpdateBoard(grid, gridHeight, gridWidth);
+                generation++;
+
+                timeSinceLastUpdate -= updateInterval;
+            }
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Gray);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.DrawString(font, (text + generation), position, Color.Blue);
+            gridDrawer.Draw(_spriteBatch, grid, 10);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public void FillGrid(int height, int width)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < height; i++)
+            {
+                List<int> row = new List<int>();
+                for (int j = 0; j < width; j++)
+                {
+                    // 33.33% chance of being alive
+                    if (rnd.Next(0, 3) == 0)
+                    {
+                        row.Add(1);
+                    }
+                    else
+                    {
+                        row.Add(0);
+                    }
+                }
+                grid.Add(row);
+            }
+        }
+    }
+}
---
+++ Project1/Game1source/GridDrawer.cs
@@ -0,0 +1,244 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class GridDrawer
+{
+    private Texture2D pixel;
+    private GraphicsDevice graphicsDevice;
+
+    public GridDrawer(GraphicsDevice graphicsDevice)
+    {
+        this.graphicsDevice = graphicsDevice;
+        pixel = new Texture2D(graphicsDevice, 1, 1);
+        pixel.SetData(new[] { Color.White });
+    }
+
+    public void Draw(SpriteBatch spriteBatch, List<List<int>> grid, int cellSize)
+    {
+        for (int y = 0; y < grid.Count; y++)
+        {
+            for (int x = 0; x < grid[y].Count; x++)
+            {
+                var position = new Vector2(x * cellSize, y * cellSize);
+                var rectangle = new Rectangle((int)position.X, (int)position.Y + 20, cellSize, cellSize);
+                if (grid[y][x] == 0)
+                {
+                    spriteBatch.Draw(pixel, rectangle, Color.Black);
+                }
+                else if (grid[y][x] == 1)
+                {
+                    spriteBatch.Draw(pixel, rectangle, Color.White);
+                }
+                /*spriteBatch.Draw(pixel, new Rectangle(rectangle.X, rectangle.Y, 1, rectangle.Height), Color.DarkGray);
+                spriteBatch.Draw(pixel, new Rectangle(rectangle.X, rectangle.Y, rectangle.Width, 1), Color.DarkGray);
+                spriteBatch.Draw(pixel, new Rectangle(rectangle.X + rectangle.Width - 1, rectangle.Y, 1, rectangle.Height), Color.DarkGray);
+                spriteBatch.Draw(pixel, new Rectangle(rectangle.X, rectangle.Y + rectangle.Height - 1, rectangle.Width, 1), Color.DarkGray);
+                */
+            }
+        }
+    }
+
+    public void UpdateBoard(List<List<int>> boardlist, int height, int width)
+    {
+        int neighbours;
+        List<List<int>> newBoard = new List<List<int>>();
+        List<List<int>> neighbourcount = new List<List<int>>();
+
+        // fill neighbourcount list with 0
+        for (int i = 0; i < height; i++)
+        {
+            List<int> row = new List<int>();
+            for (int j = 0; j < width; j++)
+            {
+                row.Add(0);
+            }
+            neighbourcount.Add(row);
+        }
+
+        // fill newBoard with 0
+        for (int i = 0; i < height; i++)
+        {
+            List<int> row = new List<int>();
+            for (int j = 0; j < width; j++)
+            {
+                row.Add(0);
+            }
+            newBoard.Add(row);
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                neighbours = 0;
+                if ((i == 0) && (j == 0))
+                {
+                    if (boardlist[i + 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j + 1] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+                else if ((i == (height - 1)) && (j == 0))
+                {
+                    if (boardlist[i - 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j + 1] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+                else if ((i == (height - 1)) && (j == (width - 1)))
+                {
+                    if (boardlist[i - 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j - 1] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+                else if ((i == 0) && (j == (width - 1)))
+                {
+                    if (boardlist[i + 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j - 1] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+                else if ((i != 0) && (i != (height - 1)) && (j == 0))
+                {
+                    if (boardlist[i - 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+                else if ((i == (height - 1)) && (j != 0) && (j != (width - 1)))
+                {
+                    if (boardlist[i][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j + 1] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+                else if ((i != 0) && (i != (height - 1)) && (j == (width - 1)))
+                {
+                    if (boardlist[i + 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+                else if ((i == 0) && (j != 0) && (j != (width - 1)))
+                {
+                    if (boardlist[i][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j + 1] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+                else
+                {
+                    if (boardlist[i][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j - 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i + 1][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j + 1] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j] == 1)
+                    { neighbours++; }
+                    if (boardlist[i - 1][j - 1] == 1)
+                    { neighbours++; }
+
+                    neighbourcount[i][j] = neighbours;
+                }
+            }
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if ((boardlist[i][j] == 0) && (neighbourcount[i][j] == 3))
+                {
+                    newBoard[i][j] = 1;
+                }
+                else if ((boardlist[i][j] == 1) && (neighbourcount[i][j] < 2))
+                {
+                    newBoard[i][j] = 0;
+                }
+                else if ((boardlist[i][j] == 1) && ((neighbourcount[i][j] == 2) || (neighbourcount[i][j] == 3)))
+                {
+                    newBoard[i][j] = 1;
+                }
+                else if ((boardlist[i][j] == 1) && (neighbourcount[i][j] > 3))
+                {
+                    newBoard[i][j] = 0;
+                }
+                else
+                {
+                    newBoard[i][j] = boardlist[i][j];
+                }
+
+                
+            }
+        }
+
+        // replace boardlist with newboard and clear newboard
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                boardlist[i][j] = newBoard[i][j];
+            }
+        }
+    }
+}
